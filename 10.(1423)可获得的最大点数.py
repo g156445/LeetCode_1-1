da2c8e600aa4f6b250b076f9199fa86fecb7a7b7
现阶段：秀发 <1733135862@qq.com>
2024-10-31 16:58:58 +0800

    力扣1423. 可获得的最大点数

--- 10.(1423)可获得的最大点数.py
+++ 10.(1423)可获得的最大点数.py
@@ -1,2 +1,16 @@
 class Solution:
     def maxScore(self, cardPoints, k):
+        current_score = sum(cardPoints[-k:])
+        max_score = current_score
+        for location in range(k):
+            current_score += cardPoints[location] - cardPoints[-k + location]
+            max_score = max(max_score, current_score)
+        return max_score
+
+
+cardPoints = [1,2,3,4,5,6,1]
+k = 3
+
+
+solution = Solution()
+print(solution.maxScore(cardPoints, k))
