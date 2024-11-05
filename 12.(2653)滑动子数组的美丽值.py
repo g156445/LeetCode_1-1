22d4dcee9b26402d211e899b2d57c37e20e13b32
现阶段：秀发 <1733135862@qq.com>
2024-11-05 21:52:49 +0800

    力扣2653. 滑动子数组的美丽值

--- 12.(2653)滑动子数组的美丽值.py
+++ 12.(2653)滑动子数组的美丽值.py
@@ -5,3 +5,23 @@
 子数组指的是数组中一段连续 非空 的元素序列
 """
 
+
+class Solution:
+    def getSubarrayBeauty(self, nums, k, x):
+        sub_string = []
+        for location in range(len(nums) - k + 1):
+            un_sorted_list = sorted(nums[location: k + location])
+            if un_sorted_list[x - 1] < 0:
+                sub_string.append(un_sorted_list[x - 1])
+            else:
+                sub_string.append(0)
+        return sub_string
+
+solution = Solution()
+
+nums = [1,-1,-3,-2,3]
+k = 3
+x = 2
+
+print(solution.getSubarrayBeauty(nums, k, x))
+print("------------------------------------------------------")
